4a18da2c83cedd3fcf98c86b4e22d469557e02b2
Daniel Kantor <daniel.kantor2009@gmail.com>
2020-09-07 12:23:56 -0700

    Adding	Chapter_1.sln, Excercise1-1.cpp and Excercise1-1.vcxproj files

---
+++ Chapter_1/Chapter_1/Excercise1-1/Excercise1-1.cpp
@@ -0,0 +1,9 @@
+// Excercise1-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "This is a placeholder for excercise 1-1\n";
+}
