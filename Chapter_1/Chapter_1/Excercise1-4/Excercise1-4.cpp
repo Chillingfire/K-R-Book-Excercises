3a9768c3639a7389f39fc7a0bdc98e3607010880
Daniel Kantor <daniel.kantor2009@gmail.com>
2020-09-08 10:59:55 -0700

    Finished excercise 1-4

--- Chapter_1/Chapter_1/Excercise1-4/Excercise1-4.cpp
+++ Chapter_1/Chapter_1/Excercise1-4/Excercise1-4.cpp
@@ -5,16 +5,23 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    celsius = lower;
+
+    printf("Celsius  Fahr\n");
+
+    while (celsius <= upper) {
+        fahr = ((9.0 / 5.0) * celsius) + 32;
+        printf("%3.0f \t%6.1f\n", celsius, fahr);
+        //cout << fahr << " " << fixed << setprecision(2) << celsius << endl;
+        celsius = celsius + step;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
