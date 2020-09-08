6d77b32249279827a4acff0cebc8b676393a49d8
Daniel Kantor <daniel.kantor2009@gmail.com>
2020-09-08 16:38:07 -0700

    Finished excercises 11 and 12

---
+++ Chapter_1/Chapter_1/Excercise1-11/Excercise1-11.cpp
@@ -0,0 +1,10 @@
+// Excercise1-11.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    //Using inputs like multiple spaces, no input at all, etc.
+}
+
