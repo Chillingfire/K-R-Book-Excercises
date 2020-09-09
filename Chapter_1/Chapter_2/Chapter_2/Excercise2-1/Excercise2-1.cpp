97a2d932e177a69547702a06d486e71621034b95
Daniel Kantor <daniel.kantor2009@gmail.com>
2020-09-09 12:44:30 -0700

    Finished excercise2-1

---
+++ Chapter_1/Chapter_2/Chapter_2/Excercise2-1/Excercise2-1.cpp
@@ -0,0 +1,20 @@
+// Excercise2-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    printf("Minimum of char: %d\n", SCHAR_MIN);
+    printf("Maximum of char: %d\n\n", SCHAR_MAX);
+
+    printf("Minimum of short: %d\n", SHRT_MIN);
+    printf("Maximum of short: %d\n\n", SHRT_MAX);
+
+    printf("Minimum of int: %d\n", INT32_MIN);
+    printf("Maximum of int: %d\n\n", INT32_MAX);
+
+    printf("Minimum of long: %d\n", LONG_MIN);
+    printf("Maximum of long: %d\n", LONG_MAX);
+}
+
