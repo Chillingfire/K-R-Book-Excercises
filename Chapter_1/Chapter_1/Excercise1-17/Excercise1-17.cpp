e46efc2b8a82bc3cfa4a0b56ced58be3f141354c
Daniel Kantor <daniel.kantor2009@gmail.com>
2020-09-09 16:36:32 -0700

    Finished excercise1-17

--- Chapter_1/Chapter_1/Excercise1-17/Excercise1-17.cpp
+++ Chapter_1/Chapter_1/Excercise1-17/Excercise1-17.cpp
@@ -2,8 +2,39 @@
 //
 
 #include <iostream>
+#define MAX_LENGTH 1000
+#define ALPHABET_LENGTH 26
 
 int main()
 {
-   //Work on it later
+	char userString[MAX_LENGTH];
+	char returnedString[MAX_LENGTH];
+	char c;
+	int chars = 0;
+
+	for (int z = 0; z < MAX_LENGTH; z++) {
+		userString[z] = ' ';
+	}
+
+	while ((c = getchar()) != EOF) {
+		userString[chars] = c;
+		chars++;
+		
+		if (c == '\n') {
+			if (chars > 10) {
+				for (int i = 0; i < chars; i++) {
+					printf("%c", userString[i]);
+				}
+
+				for (int i = 0; i < chars; i++) {
+					userString[i] = ' ';
+				}
+			}
+			else {
+				chars = 0;
+			}
+			
+			chars = 0;
+		}	
+	}
 }
