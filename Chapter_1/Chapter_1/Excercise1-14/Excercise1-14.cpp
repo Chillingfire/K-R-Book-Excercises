121d85b5be3eb870d51ac510ed12ee13e74583f7
Daniel Kantor <daniel.kantor2009@gmail.com>
2020-09-09 14:30:46 -0700

    Finished excercise1-14

--- Chapter_1/Chapter_1/Excercise1-14/Excercise1-14.cpp
+++ Chapter_1/Chapter_1/Excercise1-14/Excercise1-14.cpp
@@ -2,9 +2,36 @@
 //
 
 #include <iostream>
+#define ALPHABET_LENGTH 26
 
 int main()
 {
-    //Come back to later
+    char alphabet[ALPHABET_LENGTH] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+    int charOccurences[ALPHABET_LENGTH];
+
+    for (int i = 0; i < ALPHABET_LENGTH; i++) {
+        charOccurences[i] = 0;
+    }
+
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        for (int i = 0; i < ALPHABET_LENGTH; i++) {
+            if (c == alphabet[i]) {
+                charOccurences[i] += 1;
+            }
+        }
+
+        if (c == '\n') {
+            for (int i = 0; i < ALPHABET_LENGTH; i++) {
+                printf("%c:", alphabet[i]);
+                for (int x = 0; x < charOccurences[i]; x++) {
+                    printf("0");
+                }
+                printf("\n");
+            }
+        }
+    }
 }
 
