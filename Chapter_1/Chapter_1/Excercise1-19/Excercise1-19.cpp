61eb5ac4fe36a940eff9868da5da58019d98e3d5
Daniel Kantor <daniel.kantor2009@gmail.com>
2020-09-12 12:57:14 -0700

    Finished excercise1-19

--- Chapter_1/Chapter_1/Excercise1-19/Excercise1-19.cpp
+++ Chapter_1/Chapter_1/Excercise1-19/Excercise1-19.cpp
@@ -2,9 +2,48 @@
 //
 
 #include <iostream>
+#define MAX 1000
+
+void Reverse(char s[]);
 
 int main()
 {
-    
+    char s[MAX];
+
+    Reverse(s);
 }
 
+void Reverse(char s[]) 
+{
+    char c;
+    int character = 0;
+    int otherCharacter = 0;
+    char finalArray[MAX];
+
+    while ((c = getchar()) != EOF) {
+        s[character] = c;
+        character++;
+
+        if (c == '\n') {
+
+            character -= 1;
+
+            for (int i = character; i > -1; i--) {
+                finalArray[otherCharacter] = s[i];
+                otherCharacter++;
+            }
+
+            finalArray[otherCharacter] = '\0';
+
+            printf("%s", finalArray);
+
+            for (int i = 0; i < MAX; i++) {
+                s[i] = '\0';
+            }
+            printf("\n\n");
+
+            character = 0;
+            otherCharacter = 0;
+        }
+    }
+}
