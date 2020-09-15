1bb711b8bf132cd22e04a76e987497b4107e8374
Daniel Kantor <daniel.kantor2009@gmail.com>
2020-09-14 18:34:32 -0700

    Finished excercise1-21

--- Chapter_1/Chapter_1/Excercise1-21/Excercise1-21.cpp
+++ Chapter_1/Chapter_1/Excercise1-21/Excercise1-21.cpp
@@ -2,19 +2,44 @@
 //
 
 #include <iostream>
+#define MAX 1000
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char c;
+    int character = 0;
+    int blanks = 0;
+    char lineArray[MAX];
+
+    while ((c = getchar()) != EOF) {
+        lineArray[character] = c;
+        character++;
+
+        if (c == '\n') {
+            for (int i = character; i >= 0; i--) {
+                if (lineArray[i] == ' ') {
+                    blanks++;
+                }
+            }
+
+            while (blanks > 0) {
+                if (blanks > 2) {
+                    printf("\\t");
+                    blanks -= 3;
+                }
+                else {
+                    printf("_");
+                    blanks--;
+                }
+            }
+
+            for (int i = 0; i < 1000; i++) {
+                lineArray[i] = '\0';
+            }
+
+            putchar('\n');
+            character = 0;
+        } 
+    }      
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
