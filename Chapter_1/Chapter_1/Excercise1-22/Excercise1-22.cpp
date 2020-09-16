705b9c2d278a5a54c97500a95263b262c2809232
Daniel Kantor <daniel.kantor2009@gmail.com>
2020-09-16 15:00:28 -0700

    Finished excercise1-22

--- Chapter_1/Chapter_1/Excercise1-22/Excercise1-22.cpp
+++ Chapter_1/Chapter_1/Excercise1-22/Excercise1-22.cpp
@@ -2,19 +2,49 @@
 //
 
 #include <iostream>
-
+#define MAX 1000
 int main()
 {
-    std::cout << "Hello World!\n";
+    char c;
+    int i = 0; //used for locations in lineArray
+    int n = 14; //used as 'nth'
+    int chars = 0;
+    char lineArray[MAX];
+
+    while ((c = getchar()) != EOF) {
+        lineArray[chars] = c;
+        chars++;
+
+        if (c == '\n') {
+            lineArray[chars + 1] = '\0';
+            putchar('\n');
+
+            while (i < chars) {
+                putchar(lineArray[i]);
+                i++;
+
+                if (i % n == 0) {
+                    while (lineArray[i] != ' ') {
+                        putchar(lineArray[i]);
+                        i++;  
+                        if (i == chars) {
+                            break;
+                        }
+                    }
+                    
+                    putchar('\n');
+                    i++;
+                }
+            }
+
+            for (int i = 0; i < 1000; i++) {
+                lineArray[i] = '\0';
+            }
+
+            putchar('\n');
+            chars = i = 0;
+        }
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
